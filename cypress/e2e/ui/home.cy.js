5ba1113df6fcd78dae46a08c55d576d593cf0235
Denis <deniznagaev@yandex.ru>
2026-06-07 00:27:25 +0500

    Add Cypress UI interaction tests

--- cypress/e2e/ui/home.cy.js
+++ cypress/e2e/ui/home.cy.js
@@ -16,5 +16,48 @@ describe('Home page', () => {
     cy.url().should('include', 'example.cypress.io');
   });
 
+  it('should navigate to querying page', () => {
+    cy.get('a[href="/commands/querying"]')
+    .filter(':visible')
+    .first()
+    .click();
+    cy.url().should('include', '/commands/querying');
+    cy.get('h1').should('contain', 'Querying');
+  });
+
+  it('should type email into input field', () => {
+    cy.get('a[href="/commands/actions"]')
+     .filter(':visible')
+     .first()
+     .click();
+
+     cy.url().should('include', '/commands/actions');
+
+     cy.get('.action-email')
+      .type('test@example.com')
+      .should('have.value', 'test@example.com');
+  });
+
+  it('should submit form', () => {
+    cy.get('a[href="/commands/actions"]')
+     .filter(':visible')
+     .first()
+     .click();
+     
+    cy.url().should('include', '/commands/actions');
+     
+    cy.get('.action-form').as('actionForm');
+     
+    cy.get('@actionForm')
+     .find('[type="text"]')
+     .type('HALFOFF');
+
+    cy.get('@actionForm')
+    .submit();
+
+    cy.get('@actionForm')
+     .next()
+     .should('contain', 'Your form has been submitted!');
+  });
 });
 
