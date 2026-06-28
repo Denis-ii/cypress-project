6e223a2cb2a057d26842a5d82b8d8744746dfbce
Denis <deniznagaev@yandex.ru>
2026-06-29 03:42:49 +0500

    Add Playwright locator and fill tests

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -2,5 +2,22 @@ const { test, expect } = require('@playwright/test');
 
 test('should open Cypress example page', async ({page}) => {
     await page.goto('https://example.cypress.io');
+
     await expect(page.getByText('Kitchen Sink')).toBeVisible()
+});
+
+test('should open Querying page', async ({ page }) => {
+  await page.goto('https://example.cypress.io');
+
+  await page.locator('a[href="/commands/querying"]:visible').first().click();
+
+  await expect(page).toHaveURL(/.*commands\/querying/);
+});
+
+test('should fill email input', async ({ page }) => {
+  await page.goto('https://example.cypress.io/commands/actions');
+
+  await page.locator('.action-email').fill('test@example.com');
+
+  await expect(page.locator('.action-email')).toHaveValue('test@example.com');
 });
